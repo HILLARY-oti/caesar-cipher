c8cdf6378f9429af6d481321d935ca9ec97c997f
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-27 15:03:38 +0300

    add test on letter

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,1 +1,12 @@
-public
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CipherTest {
+
+    @Test
+    public void Cipher_changePositionOfLetter_String() {
+        Cipher testCipher = new Cipher();
+        String expectedOutput = "w";
+        assertEquals(expectedOutput, testCipher.encrypt("t", 3));
+    }
+}
