2122ec65711948ee4ed9bf0ae6c46574c24bad2e
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-28 15:10:09 +0300

    add user input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
+
+import java.io.Console;
 
 public class App {
     public static void main(String[] args) {
-        Scanner myConsole = new Scanner(System.in);
+        Console myConsole = System.console();
         System.out.println("Welcome to Ceaser Cipher");
         System.out.println("");
 
@@ -11,28 +12,31 @@ public class App {
         System.out.println("\tEnter 3 to exit\n");
         System.out.println("");
         System.out.println("Enter Operation to be executed:");
-        int value = myConsole.nextInt();
+        String value = myConsole.readLine();
 
 
         Cipher ciphered = new Cipher();
         Decipher deciphered = new Decipher();
 
-        if (value == 1) {
-            System.out.println("Enter Text to cipher");
-            String input = myConsole.next();
-            System.out.println("Enter Key");
-            int key = myConsole.nextInt();
-            System.out.println("Your encrypted message:" + ciphered.encrypt(input, key));
-        } else if (value == 2) {
-            System.out.println("Enter Text to decipher");
-            String input = myConsole.next();
-            System.out.println("Enter Key");
-            int key = myConsole.nextInt();
-            System.out.println("Your encrypted message:" + deciphered.decrypt(input, key));
-        } else {
-            System.out.println("Exiting");
-            System.exit(0);
-        }
+            if (value.equals("1")) {
+                System.out.println("Enter Text to cipher");
+                String input = myConsole.readLine();
+                System.out.println("Enter Key");
+                String userNumber = myConsole.readLine();
+                int key = Integer.parseInt(userNumber);
+                System.out.println("Your encrypted message: " + ciphered.encrypt(input, key));
+
+            } else if (value.equals("2")) {
+                System.out.println("Enter Text to decipher");
+                String input = myConsole.readLine();
+                System.out.println("Enter Key");
+                String userNumber = myConsole.readLine();
+                int key = Integer.parseInt(userNumber);
+                System.out.println("Your encrypted message: " + deciphered.decrypt(input, key));
+            } else {
+                System.out.println("Exiting");
+                System.exit(0);
+            }
 
     }
 }
